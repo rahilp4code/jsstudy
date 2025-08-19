34df3bcb5105671f2a04dc5c32d657ca0d41a702
rahilp4code <pathanrahil2244@gmail.com>
2025-08-20 00:07:18 +0530

    File name fix

--- 3.ArrayMethods/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const monthsInYear = ["Jan", "March", "April", "June"];
-monthsInYear.splice(0, 1);
-const h123213 = monthsInYear.splice(0, 1);
-// Inserts at index 1
-console.log(monthsInYear);
-console.log(h123213);
